83e35b95881364e2333d6e62451fed0b5728a713
work <niall.brady@aquaq.co.uk>
2022-03-05 01:35:43 +0000

    Update git-push.bat
    Added more functionality to git-push.bat

--- git-push.bat
+++ git-push.bat
@@ -1,11 +1,22 @@
+:choose
+@echo on
+git status
 @echo off
+set /p choice="Enter a file to add to this commit (Leave empty to move on to committing): "
+IF NOT "%choice%" == "" git add "%choice%" && set "choice=" && goto :choose
+
+:askcomment
 set /p cmnt="Enter a commit comment: "
+IF "%cmnt%" == "" echo "ERROR: No comment given!" && echo[ && goto :askcomment
+set /p desc="Enter a commit description: "
 cd %~dp0
 
 @echo on
-git add .
 git status
-git commit -m "%cmnt%"
+@echo off
+IF "%desc%" == "" @echo on && git commit -m "%cmnt%"
+@echo off
+IF NOT "%desc%" == "" @echo on && git commit -m "%cmnt%" -m "%desc%"
 git push origin main
 
 @echo off
